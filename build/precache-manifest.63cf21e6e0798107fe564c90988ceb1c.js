c308f277d2f710b9ae34fe2e3e68d6c8b8d26536
xavier birnie-scott <xavier_bs@murviel-info.com>
2019-07-18 20:03:40 +0200

    Add build folder
    Build folder

---
+++ build/precache-manifest.63cf21e6e0798107fe564c90988ceb1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b72bfbc9ff4f0bdc04851dfb576cc6eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a118631d2f8992f7fc78",
+    "url": "/static/css/main.c2550e0a.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "a118631d2f8992f7fc78",
+    "url": "/static/js/main.f245a579.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
